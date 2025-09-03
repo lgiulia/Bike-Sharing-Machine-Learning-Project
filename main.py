55afe792f44ad09caa0066decb4b90451308bb67
Giulia <giulialui02@gmail.com>
2025-09-03 15:13:00 +0200

    updated

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# Variabili da ignorare: instant, casual, registered
-# Variabili categoriche: season, yr, mnth, hr, weekday, weathersit
-# Variabili binarie: holiday, workingday
-# Variabili numeriche: temp, atemp, hum, windspeed
-# Variabile da trasformare: dteday (usata per estrarre weekday o mnth) ma si può IGNORARE perchè ci sono già nel dataset
-
 import pandas as pd
 import warnings
 import seaborn as sns
@@ -22,7 +16,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=UserWarning)
 
 # Caricamento del dataset
-df = pd.read_csv('hour.csv') #df: dataframe
+df = pd.read_csv('hour.csv')
 
 print(df.info()) #riepilogo del dataframe
 print('\nNull elements: ', df.isnull().values.any()) #verifica l'esistenza di valori mancanti
@@ -43,11 +37,11 @@ print("\nDataframe dopo One-Hot Encoding:")
 print(X.info())
 
 # Fase 2: Visualizzazione dei dati
-sns.histplot(x='cnt', data=df, kde=True) #crea un istogramma. kde (Kernel Density Estimate):linea curva
+sns.histplot(x='cnt', data=df, kde=True) #crea un istogramma
 plt.title('Distribution of Bike Rentals')
 plt.show()
 
-sns.scatterplot(x='temp', y='cnt', data=df) #crea un grafico a dispersione. Ha una relazione non lineare
+sns.scatterplot(x='temp', y='cnt', data=df) #crea un grafico a dispersione
 plt.title('Bike Rentals vs. Temperature')
 plt.show()
 
@@ -91,9 +85,7 @@ estimators = [] # per salvare i migliori modelli
 
 for model, model_name, hparameters in zip(models, models_names, models_parameters):
     print(f'\n{model_name}')
-    clf = GridSearchCV(estimator=model, param_grid=hparameters, scoring='neg_mean_squared_error', cv=5) #estimator: algoritmo che si vuole ottimizzare; param_grid: iperparametri che si vogliono utilizzare;
-                                                                                                        #scoring: metrica di valutazione (massimizzare il valore negativo ovvero minimizzare il valore positivo);
-                                                                                                        #cv: numero di fold (pieghe) per la cross-validation
+    clf = GridSearchCV(estimator=model, param_grid=hparameters, scoring='neg_mean_squared_error', cv=5)
     clf.fit(X_train_scaled, y_train) # avvia training e ottimizzazione
     choosen_hparameters.append(clf.best_params_) # combinazione di iperparametri che ha dato il miglior punteggio
     estimators.append((model_name, clf.best_estimator_)) # modello già addestrato con la migliore combinazione di parametri
@@ -108,10 +100,7 @@ print("\n--- Stacking Regressor ---")
 final_model = StackingRegressor(estimators=estimators, final_estimator=LinearRegression()) # Stacking Regressor: combina le previsioni di più modelli di base per creare una previsione finale
 
 # Cross Validation per il modello finale
-scores = cross_validate(final_model, X_train_scaled, y_train, cv=5, scoring='neg_root_mean_squared_error') # final_model: modello che viene addestrato e valutato (Stacking Regressor);
-                                                                                                            # X_train_scaled e y_train: dati che vengono utilizzati per la cross-validation (solo training no testing);
-                                                                                                            # cv=5: 5 fold (il set di training viene diviso in 5 sotto-insiemi, addestrato su 4 e valutato sul quinto
-                                                                                                            # scoring: metrica di valutazione valore negativo di RMSE
+scores = cross_validate(final_model, X_train_scaled, y_train, cv=5, scoring='neg_root_mean_squared_error')
 rmse_scores = -scores['test_score'] # accede alla chiave 'test_score' nel diz score che contiene i punteggi di RMSE per ogni fold
 print(f'The cross-validated RMSE of the Stacking Ensemble meta-model is {np.mean(rmse_scores): .2f}')
 
@@ -132,7 +121,7 @@ print(f'R-squared (R2) Score: {r2: .2f}')
 # Visualizzazione dei risultati
 plt.figure(figsize=(12, 10))
 sns.scatterplot(x=y_test, y=y_pred)
-plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=2) # Traccia una linea sul grafico che su x e y va dal valore minimo al valore massimo di y_test; r--: riga rossa tratteggiata; lw=2: line width
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=2) 
 plt.xlabel('Valori reali')
 plt.ylabel('Valori Predetti')
 plt.title('Valori Reali vs Valori Predetti')
